48761234ce196dd904fccd83e32cdedc17c69fd9
Dima Krasner <dima@dimakrasner.com>
2020-06-11 20:02:03 +0300

    add cross-builds to CI

--- ci.sh
+++ ci.sh
@@ -2,3 +2,9 @@
 
 meson --buildtype=release build
 ninja -C build
+
+for i in arm-any32-linux-musleabi mips-any32-linux-musl
+do
+	meson --buildtype=release --cross-file=$i build-$i
+	ninja -C build-$i
+done
