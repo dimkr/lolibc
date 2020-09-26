cde2b8a3b2df2a7e57d859ce960bb9cf8b64fe53
Dima Krasner <dima@dimakrasner.com>
2020-09-26 19:07:40 +0300

    CHILD_MAX must not be negative

--- include/limits.h
+++ include/limits.h
@@ -1,6 +1,22 @@
 #include_next <limits.h>
 
 #ifndef CHILD_MAX
+
 #include <unistd.h>
-#define CHILD_MAX sysconf(_SC_CHILD_MAX)
+
+static inline int _get_child_max(void)
+{
+	static long cache;
+
+	if (cache != 0) {
+		cache = sysconf(_SC_CHILD_MAX);
+		if ((cache <= 0) || (cache > INT_MAX))
+			cache = INT_MAX;
+	}
+
+	return (int)cache;
+}
+
+#define CHILD_MAX _get_child_max()
+
 #endif
