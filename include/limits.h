0d51bb906b8a983d9fea0c3ff21f92e6781f6bbd
Dima Krasner <dima@dimakrasner.com>
2020-09-26 19:19:21 +0300

    bad condition

--- include/limits.h
+++ include/limits.h
@@ -8,7 +8,7 @@ static inline int _get_child_max(void)
 {
 	static long cache;
 
-	if (cache != 0) {
+	if (cache == 0) {
 		cache = sysconf(_SC_CHILD_MAX);
 		if ((cache <= 0) || (cache > INT_MAX))
 			cache = INT_MAX;
