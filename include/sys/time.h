22d535be0fb5ea45f6d314ca90a613e10effbc31
Dima Krasner <dima@dimakrasner.com>
2021-05-01 11:52:23 +0300

    update to 6.9

--- include/sys/time.h
+++ include/sys/time.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: time.h,v 1.55 2020/07/06 13:33:09 pirofti Exp $	*/
+/*	$OpenBSD: time.h,v 1.58 2021/01/13 16:28:50 cheloha Exp $	*/
 /*	$NetBSD: time.h,v 1.18 1996/04/23 10:29:33 mycroft Exp $	*/
 
 /*
